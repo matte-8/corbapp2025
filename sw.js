49515511bfe50997af2838bd365c2fafb1068aaa
matte-8 <abbb.biii.biii@gmail.com>
2026-07-25 19:52:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — cache offline (PWA) + notifiche push in background (Firebase Cloud Messaging)
-const CACHE = "corb-cache-v11";
+const CACHE = "corb-cache-v12";
 
 const ASSETS = [
   "./",
