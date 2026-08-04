44eb8dbaaa17a451b982da5bacbf6d0e58865c55
corbapp <abbb.biii.biii@gmail.com>
2026-08-04 22:15:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — cache offline (PWA) + notifiche push in background (Firebase Cloud Messaging)
-const CACHE = "corb-cache-v54";
+const CACHE = "corb-cache-v56";
 
 const ASSETS = [
   "./",
